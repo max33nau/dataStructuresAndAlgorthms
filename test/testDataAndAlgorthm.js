2229ed565e3b4677fadfc235ee5e82a7f8cf55e5
max33nau <mtj29@nau.edu>
2016-01-10 18:25:38 -0800

    unique and frequency function now works

--- test/testDataAndAlgorthm.js
+++ test/testDataAndAlgorthm.js
@@ -13,11 +13,19 @@ describe('test our array functions', function() {
     });
     it('should shift the first value out of the array', function() {
       var testArray = [1, 2, 3];
-      assert.deepEqual(myShift(testArray), 1, 'did not pass')
-    })
+      assert.deepEqual(myShift(testArray), 1, 'did not pass');
+    });
     it('should unshift(push) value into first index of array', function() {
       var testArray = [1, 2, 3];
-      assert.deepEqual(myUnshift(testArray, 'foo'), ['foo', 1, 2, 3], 'did not pass')
+      assert.deepEqual(myUnshift(testArray, 'foo'), ['foo', 1, 2, 3], 'did not pass');
+    });
+    it('should take a array and return a copy of the array with all the unique values', function(){
+      var testArray = [1,2,3,2,4,4];
+      assert.deepEqual(unique(testArray), [1,2,3,4], 'did not pass');
+    });
+    it('should return the total amount of times a specified letter returns in a array of words', function(){
+      var testArray = ['foo', 'max', 'code', 'foobuzz', 'buzz'];
+      assert.deepEqual(frequency(testArray,'o'), 3, 'did not pass');
     })
 
 });
@@ -41,6 +49,7 @@ function myPush(array, val) {
 }
 /* ALEX DRIVER
 MAX NAVIGATOR */
+/* Shift */
 function myShift(array) {
     var length = array.length;
     var shiftVal = array[0];
@@ -50,6 +59,7 @@ function myShift(array) {
     myPop(array);
     return shiftVal;
 }
+/* Unshift */
 function myUnshift(array, val) {
   var length = array.length;
   array.length = length+1;
@@ -59,3 +69,36 @@ function myUnshift(array, val) {
   array[0] = val
   return array;
 };
+
+/* No more Pair Programming for these next two */
+/* unique */
+function unique(array) {
+  array.sort();
+  for (var ii = 0; ii < array.length; ii++) {
+    if (array[ii] == array[ii + 1]) {
+      array.splice(ii,1);
+    }
+  }
+  return array;
+};
+
+/* frequency */
+function foundLetter(word, letter) {
+  var letterArray = word.split('');
+  for(var ii = 0; ii < letterArray.length; ii++) {
+    if(letterArray[ii] == letter) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function frequency(array, specifiedLetter) {
+  var numberOfWordsWithLetter = 0;
+  for (var ii = 0; ii < array.length; ii++) {
+    if ( foundLetter(array[ii], specifiedLetter) ) {
+      numberOfWordsWithLetter++;
+    }
+  }
+  return numberOfWordsWithLetter;
+}
